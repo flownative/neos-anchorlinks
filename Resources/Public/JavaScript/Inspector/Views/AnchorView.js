8e462a5d98ac641476e3abbf12599cbacdd95be4
Raffael Comi <RaffaelComi@web.de>
2017-01-30 10:17:00 +0100

    !!! TASK: Upgrade for Neos 3.0 (#3)
    - Adjusts code to package renaming from "TYPO3.Neos" to "Neos.Neos"
    - Adjusts code to package renaming from "TYPO3.TypoScript" to "Neos.Fusion" in Fusion files
    - Moves fusion files from old path ``Resources/Private/TypoScript/`` to the new path
    - Migrate usages of the Settings path Neos.Neos.typoScript to Neos.Neos.fusion

--- Resources/Public/JavaScript/Inspector/Views/AnchorView.js
+++ Resources/Public/JavaScript/Inspector/Views/AnchorView.js
@@ -1,32 +1,32 @@
 define([
-	'emberjs',
-	'text!./AnchorView.html',
-	'Shared/I18n'
+    'emberjs',
+    'text!./AnchorView.html',
+    'Shared/I18n'
 ],
 function(
-	Ember,
-	template,
-	I18n
+    Ember,
+    template,
+    I18n
 ) {
-	return Ember.View.extend({
-		template: Ember.Handlebars.compile(template),
-		buttonLabel: 'copy',
-		_buttonLabel: function() {
-			return I18n.translate('Flownative.Anchorlinks:Main:' + this.get('buttonLabel'), 'Copy link');
-		}.property('buttonLabel'),
-		_showButton: function() {
-			return this.get('inspector.nodeProperties.sectionId') ? true : false;
-		}.property('inspector.nodeProperties.sectionId'),
-		copyToClipboard: function () {
-			var documentNodeIdentifier = document.getElementById('neos-document-metadata').dataset['node-_identifier'];
-			var link = 'node://' + documentNodeIdentifier + '#' + this.get('inspector.nodeProperties.sectionId');
-			var textArea = document.createElement('textarea');
-			textArea.innerText = link;
-			document.body.appendChild(textArea);
-			textArea.select();
-			document.execCommand('copy');
-			textArea.parentNode.removeChild(textArea);
-			this.set('buttonLabel', 'copied');
-		}
-	});
+    return Ember.View.extend({
+        template: Ember.Handlebars.compile(template),
+        buttonLabel: 'copy',
+        _buttonLabel: function() {
+            return I18n.translate('Flownative.Anchorlinks:Main:' + this.get('buttonLabel'), 'Copy link');
+        }.property('buttonLabel'),
+        _showButton: function() {
+            return !!this.get('inspector.nodeProperties.sectionId');
+        }.property('inspector.nodeProperties.sectionId'),
+        copyToClipboard: function () {
+            var documentNodeIdentifier = document.getElementById('neos-document-metadata').dataset['node-_identifier'];
+            var link = 'node://' + documentNodeIdentifier + '#' + this.get('inspector.nodeProperties.sectionId');
+            var textArea = document.createElement('textarea');
+            textArea.innerText = link;
+            document.body.appendChild(textArea);
+            textArea.select();
+            document.execCommand('copy');
+            textArea.parentNode.removeChild(textArea);
+            this.set('buttonLabel', 'copied');
+        }
+    });
 });
