d193b492c608f4f7cc55ee8af26568f3c4d23a61
Roland Schütz <mail@rolandschuetz.at>
2021-09-18 15:22:59 +0200

    BUGFIX: Consistent naming

--- Resources/Private/JavaScript/AnchorView/src/manifest.js
+++ Resources/Private/JavaScript/AnchorView/src/manifest.js
@@ -1,7 +1,7 @@
 import manifest from '@neos-project/neos-ui-extensibility';
 import AnchorView from './AnchorView';
 
-manifest('Flownative.AnchorLinks:AnchorView', {}, globalRegistry => {
+manifest('Flownative.Anchorlinks:AnchorView', {}, globalRegistry => {
     const viewsRegistry = globalRegistry.get('inspector').get('views');
 
     viewsRegistry.set('Flownative.Anchorlinks/Views/AnchorView', {
